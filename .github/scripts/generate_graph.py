9d7928b2b3ae1f5615e7a181b1865133d542be47
NiaoBlush <303138518@qq.com>
2024-08-07 22:42:15 +0800

    绘图修正

--- .github/scripts/generate_graph.py
+++ .github/scripts/generate_graph.py
@@ -13,6 +13,8 @@ plt.rcParams['axes.unicode_minus'] = False  # 正常显示负号
 with open('install_counts.json') as f:
     data = json.load(f)
 
+print("1111111111111111111111111")
+
 # 提取日期和数据
 dates = list(data.keys())
 installs = [data[date]['installs'] for date in dates]
@@ -24,19 +26,29 @@ cumulative_installs = np.cumsum(installs)
 # 将日期转换为数字
 x = np.arange(len(dates))
 
+# 创建平滑曲线
+if len(dates) > 3:  # 确保数据点足够多进行平滑处理
+    x_smooth = np.linspace(x.min(), x.max(), 300)
+    installs_smooth = make_interp_spline(x, cumulative_installs)(x_smooth)
+    update_checks_smooth = make_interp_spline(x, update_checks)(x_smooth)
+else:
+    x_smooth = x
+    installs_smooth = cumulative_installs
+    update_checks_smooth = update_checks
+
 # 计算标签间隔
 max_labels = 10  # 可以根据需要调整最大标签数
 label_interval = max(1, len(dates) // max_labels)
 
 # 绘制图表
 plt.figure(figsize=(10, 6))
-plt.plot(x, cumulative_installs, marker='', linestyle='-', label='累计安装量')
-plt.plot(x, update_checks, marker='', linestyle='-', label='检查更新次数')
-plt.xlabel('日期', fontproperties=font_prop)
-plt.ylabel('数量', fontproperties=font_prop)
-plt.title('每日累计安装量和检查更新次数', fontproperties=font_prop)
-plt.xticks(ticks=x[::label_interval], labels=dates[::label_interval], rotation=45, fontproperties=font_prop)
-plt.legend(prop=font_prop)
+plt.plot(x_smooth, installs_smooth, marker='', linestyle='-', label='累计安装量')
+plt.plot(x_smooth, update_checks_smooth, marker='', linestyle='-', label='检查更新次数')
+plt.xlabel('日期')
+plt.ylabel('数量')
+plt.title('每日累计安装量和检查更新次数')
+plt.xticks(ticks=x[::label_interval], labels=dates[::label_interval], rotation=45)
+plt.legend()
 plt.grid(True)
 
 # 保存图表
